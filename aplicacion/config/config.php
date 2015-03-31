aa5c77703fd968fc8f98524c2dafc32701a461c6
Ana Campos <anacampos.antequera@gmail.com>
2015-03-31 17:46:14 +0200

    Modificacion del config para la bd

--- aplicacion/config/config.php
+++ aplicacion/config/config.php
@@ -2,11 +2,11 @@
 
 	$config=array("CONTROLADOR"=> array("inicial"),
 				  "RUTAS_INCLUDE"=>array("aplicacion/modelos"),
-				  "BD"=>array("hay"=>false,
+				  "BD"=>array("hay"=>true,
 				              "servidor"=>"localhost",
 							  "usuario"=>"operario",
-							  "contra"=>"oper1",
-							  "basedatos"=>"relacion6"),
+							  "contra"=>"oper",
+							  "basedatos"=>"centro_deportivo"),
 				   "SESION"=>array("controlAutomatico"=>true),
 				   "ACCESO"=>array("controlAutomatico"=>true));
 
