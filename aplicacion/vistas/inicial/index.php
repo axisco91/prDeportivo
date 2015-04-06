500504df1e5fa04243bf761e7c52ee85d974150f
axisco91 <darthxisco@gmail.com>
2015-04-06 20:29:01 +0200

    Update index.php

--- aplicacion/vistas/inicial/index.php
+++ aplicacion/vistas/inicial/index.php
@@ -1,10 +1,50 @@
-mi primera aplicacion<br />
-esto se escribe tambien
-
-								
 
 
-				
-	
-
-
+ <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item">
+          <img class="first-slide" src="imagenes/tennis.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Alquila </h1>
+              <p>Alquila una de nuestras instalaciones </p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Alquila</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="imagenes/torcal.jpg" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Excurciones.</h1>
+              <p>Apuntate a una de nuestras excurciones</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Apuntate</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item active">
+          <img class="third-slide" src="imagenes/gym.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Clases.</h1>
+              <p>Apuntate a nuestro cursos como aerobic, futbol, spinning.</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Apuntate ya</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+       <span class="sr-only">Next</span>
+      </a>
+    </div>
